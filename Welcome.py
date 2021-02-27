e9f0a4460a7a085c56650dc88ae398e6da7d895f
Daniel692 <68785978+Daniel692@users.noreply.github.com>
2021-02-27 17:18:43 +0100

    3rd

--- Welcome.py
+++ Welcome.py
@@ -2,3 +2,5 @@ import sys
 
 print('hello world')
 x = input('What is your name? ')
+print('Hello nice to meet you {}'.format(x))
+print('Did you know your python version is {}'.format(sys.version))
